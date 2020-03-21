52233709c6a2365e83f04367b83413471a6e8d9a
Jens <jens.preussner@mpi-bn.mpg.de>
2020-03-21 20:28:56 +0100

    Added headline; added url prefix Quelle

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,15 @@ const Index = ({ snacks }) => (
     <PageTitle>Kategorien</PageTitle>
     <p className="text-blue-600 my-5">Add some description here</p>
 
-    {snacks.map(({Category, Text, URL, Tags, Medium, Location}) => (
+    {snacks.map(({Category, Text, URL, Tags, Medium, Location, Headline}) => (
       <div className="max-w-sm w-full lg:max-w-full lg:flex snack-card">
         <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
         </div>
         <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
           <div className="mb-8">
-            <div className="text-gray-900 font-bold text-xl mb-2">{Category}</div>
+            <div className="text-gray-900 font-bold text-xl mb-2">{Headline}</div>
             <p className="text-gray-700 text-base">{Text}</p>
-            <a className="text-sm text-gray-600 flex items-center" href={URL} target="_blank">
+            Quelle: <a className="text-sm text-gray-600 flex items-center" href={URL} target="_blank">
               {URL}
             </a>
           </div>
