0a207d64450f553dd78bc9bb21348f2b6aaaf0e8
Jens <jens.preussner@mpi-bn.mpg.de>
2020-03-21 21:26:26 +0100

    Added headline to snacks

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,11 @@ const Index = ({ snacks }) => {
           snack.Tags.toLowerCase().includes(inputState.toLowerCase()) ||
           snack.Location.toLowerCase().includes(inputState.toLowerCase())
           ) return snack
-      }).map(({Category, Snack, URL, Tags, Medium, Location}) => (
+      }).map(({Category, Snack, URL, Tags, Medium, Location, Headline}) => (
         <div style={{margin: "2em 0"}} className="max-w-sm w-full lg:max-w-full lg:flex">
           <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
             <div className="mb-8">
-              <div className="text-gray-900 font-bold text-xl mb-2">{Category}</div>
+              <div className="text-gray-900 font-bold text-xl mb-2">{Headline}</div>
               <p className="text-gray-700 text-base">{Snack}</p>
               <a className="text-sm text-gray-600 flex items-center" href={URL} target="_blank" rel="noopener noreferrer">
                 {URL}
