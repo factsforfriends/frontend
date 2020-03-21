b81c032f087b4e11f4c1feeb20750d4f2bc2e3d4
Tobias Lehwalder <tlehwalder@posteo.de>
2020-03-21 22:30:08 +0100

    depending on isOverview flag link to snacks-page oder snack-detail-page

--- components/SnackList.js
+++ components/SnackList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import PropTypes from "prop-types";
 
-const SnackList = ({ snacks, searchTerm }) => {
+const SnackList = ({ snacks, searchTerm, isOverview }) => {
   const share = (title, url) => {
-    if (navigator.share) { 
-      navigator.share({title, url}).then(
-        () => console.log("shared")
-      ).catch(
-        e => console.error(e)
-      )
+    if (navigator.share) {
+      navigator
+        .share({ title, url })
+        .then(() => console.log("shared"))
+        .catch(e => console.error(e));
     }
-  }
+  };
   return (
     <>
       {snacks
@@ -23,54 +23,74 @@ const SnackList = ({ snacks, searchTerm }) => {
           )
             return snack;
         })
-        .map(({ Category, Snack, URL, Tags, Medium, Location, Headline }) => (
-          <div
-            style={{ margin: "2em 0" }}
-            className="max-w-sm w-full lg:max-w-full lg:flex"
-          >
-            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-              <div className="mb-8">
-                <div className="text-gray-900 font-bold text-xl mb-2">
-                  <Link href={`${Category.toLowerCase()}/snacks`}>
-                    {Headline}
-                  </Link>
+        .map(
+          ({ Category, Snack, URL, Tags, Medium, Location, Headline, ID }) => (
+            <div
+              style={{ margin: "2em 0" }}
+              className="max-w-sm w-full lg:max-w-full lg:flex"
+            >
+              <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div className="mb-8">
+                  <div className="text-gray-900 font-bold text-xl mb-2">
+                    <Link
+                      href={
+                        isOverview
+                          ? `${Category.toLowerCase()}/snacks`
+                          : `/snacks/${ID}`
+                      }
+                    >
+                      {Headline}
+                    </Link>
+                  </div>
+                  <p className="text-gray-700 text-base">{Snack}</p>
+                  <a
+                    className="text-sm text-gray-600 flex items-center"
+                    href={URL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Quelle: {URL}
+                  </a>
                 </div>
-                <p className="text-gray-700 text-base">{Snack}</p>
-                <a
-                  className="text-sm text-gray-600 flex items-center"
-                  href={URL}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Quelle: {URL}
-                </a>
-              </div>
-              <div className="flex items-center">
-                <div className="text-sm">
-                  {Category.split(" ").map(category => (
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #{category.toLowerCase()}
-                    </span>
-                  ))}
-                  {Tags.split(" ").map(tag => (
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #{tag.toLowerCase()}
-                    </span>
-                  ))}
-                  {Location.split(" ").map(location => (
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #{location.toLowerCase()}
-                    </span>
-                  ))}
+                <div className="flex items-center">
+                  <div className="text-sm">
+                    {Category.split(" ").map(category => (
+                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{category.toLowerCase()}
+                      </span>
+                    ))}
+                    {Tags.split(" ").map(tag => (
+                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{tag.toLowerCase()}
+                      </span>
+                    ))}
+                    {Location.split(" ").map(location => (
+                      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{location.toLowerCase()}
+                      </span>
+                    ))}
+                  </div>
                 </div>
-              </div>
 
-              <button onClick={() => {share(Headline, URL)}}>Share!</button>
+                <button
+                  onClick={() => {
+                    share(Headline, URL);
+                  }}
+                >
+                  Share!
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
     </>
   );
 };
 
 export default SnackList;
+
+SnackList.propTypes = {
+  snacks: PropTypes.array.isRequired,
+  searchTerm: PropTypes.string,
+  isOverview: PropTypes.bool
+};
