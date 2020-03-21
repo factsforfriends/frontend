45a039d116c6b018e249a8023f7becd8edc83b8b
Tobias Lehwalder <tlehwalder@posteo.de>
2020-03-21 22:07:45 +0100

    links headlines to category pages + adds snacklist to category-snacks page

--- components/SnackList.js
+++ components/SnackList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const SnackList = ({ snacks, searchTerm }) => {
   return (
@@ -21,7 +22,9 @@ const SnackList = ({ snacks, searchTerm }) => {
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
               <div className="mb-8">
                 <div className="text-gray-900 font-bold text-xl mb-2">
-                  {Headline}
+                  <Link href={`${Category.toLowerCase()}/snacks`}>
+                    {Headline}
+                  </Link>
                 </div>
                 <p className="text-gray-700 text-base">{Snack}</p>
                 <a
