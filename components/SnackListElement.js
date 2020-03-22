1d72fb7225c66f56c8afab21d467a53a18aadbad
Erik Langenhan <erik.langenhan@web.de>
2020-03-22 15:38:41 +0100

    switch to real images

--- components/SnackListElement.js
+++ components/SnackListElement.js
@@ -158,7 +158,8 @@ const SnackListElement = ({
 
   const sharePicWrapper = (
     <div ref={sharePicRef}>
-      <Sharepic text={Snack} source={URL}></Sharepic>
+      {/* <Sharepic text={Snack} source={URL}></Sharepic> */}
+      <img src={'https://raw.githubusercontent.com/flattenandflausch/snacks-data/master/img/' + snack.ID + '.jpg'}></img>
     </div>
   );
 
