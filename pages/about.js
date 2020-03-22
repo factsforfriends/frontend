c2031f9bd4f1f5da9d53c1e731ecab3911e09368
Tobias Lehwalder <tlehwalder@posteo.de>
2020-03-22 15:17:11 +0100

    fix about.js - add fragment

--- pages/about.js
+++ pages/about.js
@@ -9,11 +9,28 @@ const H2 = styled.h2`
 
 const About = () => {
   return (
-    <H2>Über uns</H2>
-    <p>Die Webapp Facts for Friends stellt kurze und leicht verständliche Gegenargumente (Fact-Snacks) zu kursierende Fake-News bereit.</p>
-    <p>Engagierte Bürger:innen finden dort teilbare Fact-Snacks mit denen sie Fake News in ihrem privaten Umfeld begegnen können, auch wenn es Ihnen an Wissen und/oder Zeit fehlt um selbst lange Texte zu schreiben. Als Multiplikator:innen und Informational influencers erreichen sie so mit geringem Aufwand Personen ihrem privaten Umfeld, die klassischen massenmedialen Fact-Checking-Inhalten oder politischer Kommunikation nicht vertrauen oder von solchen Inhalten überfordert sind.</p>
-    <p>Die Fact Snacks können von verifizierten Fact-Checkern wie z. B. Journalist:innen direkt eingereicht werden oder werden aus der Community generiert und dann vom Platform-Team verifiziert.</p>
-    <H2>Unser Team</H2>
+    <>
+      <H2>Über uns</H2>
+      <p>
+        Die Webapp Facts for Friends stellt kurze und leicht verständliche
+        Gegenargumente (Fact-Snacks) zu kursierende Fake-News bereit.
+      </p>
+      <p>
+        Engagierte Bürger:innen finden dort teilbare Fact-Snacks mit denen sie
+        Fake News in ihrem privaten Umfeld begegnen können, auch wenn es Ihnen
+        an Wissen und/oder Zeit fehlt um selbst lange Texte zu schreiben. Als
+        Multiplikator:innen und Informational influencers erreichen sie so mit
+        geringem Aufwand Personen ihrem privaten Umfeld, die klassischen
+        massenmedialen Fact-Checking-Inhalten oder politischer Kommunikation
+        nicht vertrauen oder von solchen Inhalten überfordert sind.
+      </p>
+      <p>
+        Die Fact Snacks können von verifizierten Fact-Checkern wie z. B.
+        Journalist:innen direkt eingereicht werden oder werden aus der Community
+        generiert und dann vom Platform-Team verifiziert.
+      </p>
+      <H2>Unser Team</H2>
+    </>
   );
 };
 
